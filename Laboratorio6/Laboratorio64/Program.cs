d648d62991a479ee54932cefee3f821b5fe72d3e
Ivan Cordoba <119004564+ivan777cordoba@users.noreply.github.com>
2025-09-26 12:49:15 +0000

    Actualizar Program.cs en Laboratorio64 y Laboratorio65 con ejemplos de manejo de excepciones

--- Laboratorio6/Laboratorio64/Program.cs
+++ Laboratorio6/Laboratorio64/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            // Lanzamos una excepción personalizada
+            throw new ArithmeticException("Accesso negado - No cumple con el criterio de edad");
+        }
+        else
+        {
+            Console.WriteLine("Acceso Concedido");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            checkAge(15);  // Esta edad provocará una excepción
+        }
+        catch (ArithmeticException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        
+        Console.WriteLine("Programa continúa...");
+        Console.ReadKey();
+    }
+}
