0c526096cbf6d01d39826f2807cf4d12e3d9b033
Ivan Cordoba <119004564+ivan777cordoba@users.noreply.github.com>
2025-09-26 12:47:53 +0000

    Agregar solución Laboratorio6 y proyectos Laboratorio61 a Laboratorio65

---
+++ Laboratorio6/Laboratorio63/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int[] myNumbers = {1, 2, 3};
+            Console.WriteLine(myNumbers[10]);  // ¡Error! El índice 10 no existe
+        }
+        catch (Exception e)  // Captura cualquier tipo de error
+        {
+            Console.WriteLine("Algo salio mal, valide el índice del arreglo");
+            Console.WriteLine("Error: " + e.Message);  // Mostramos el mensaje del error
+        }
+        finally
+        {
+            // Este bloque SIEMPRE se ejecuta, haya error o no
+            Console.WriteLine("Continuacion de la aplicacion, luego del bloque try/catch");
+        }
+        
+        Console.ReadKey();
+    }
+}
