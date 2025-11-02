7614355193d96efed34c9548d76e98a6b6b98da3
ivan777cordoba <ivan7cordoba@gmail.com>
2025-11-02 18:39:35 -0500

    Agregar proyecto ConversorMonedas completo - conversor de monedas con interfaz gráfica

---
+++ Parcial2/ConversorMonedas/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace ConversorMonedas
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        private void InitializeComponent()
+        {
+            this.lblEuro = new System.Windows.Forms.Label();
+            this.lblDolar = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.txtEuro = new System.Windows.Forms.TextBox();
+            this.txtDolar = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dgvHistorial = new System.Windows.Forms.DataGridView();
+            this.colEuro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDolar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPeso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colFecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistorial)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblEuro
+            // 
+            this.lblEuro.AutoSize = true;
+            this.lblEuro.Location = new System.Drawing.Point(40, 70);
+            this.lblEuro.Name = "lblEuro";
+            this.lblEuro.Size = new System.Drawing.Size(32, 15);
+            this.lblEuro.TabIndex = 0;
+            this.lblEuro.Text = "Euro";
+            // 
+            // lblDolar
+            // 
+            this.lblDolar.AutoSize = true;
+            this.lblDolar.Location = new System.Drawing.Point(40, 110);
+            this.lblDolar.Name = "lblDolar";
+            this.lblDolar.Size = new System.Drawing.Size(37, 15);
+            this.lblDolar.TabIndex = 1;
+            this.lblDolar.Text = "Dólar";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(40, 150);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(33, 15);
+            this.lblPeso.TabIndex = 2;
+            this.lblPeso.Text = "Peso";
+            // 
+            // txtEuro
+            // 
+            this.txtEuro.Location = new System.Drawing.Point(100, 67);
+            this.txtEuro.Name = "txtEuro";
+            this.txtEuro.Size = new System.Drawing.Size(150, 23);
+            this.txtEuro.TabIndex = 3;
+            // 
+            // txtDolar
+            // 
+            this.txtDolar.Location = new System.Drawing.Point(100, 107);
+            this.txtDolar.Name = "txtDolar";
+            this.txtDolar.Size = new System.Drawing.Size(150, 23);
+            this.txtDolar.TabIndex = 4;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(100, 147);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(150, 23);
+            this.txtPeso.TabIndex = 5;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.BackColor = System.Drawing.Color.LightGreen;
+            this.btnConvertir.Location = new System.Drawing.Point(280, 67);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(100, 35);
+            this.btnConvertir.TabIndex = 6;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = false;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.LightCoral;
+            this.btnLimpiar.Location = new System.Drawing.Point(280, 120);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(100, 35);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnGuardar.Location = new System.Drawing.Point(400, 67);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(130, 35);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar en BD";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dgvHistorial
+            // 
+            this.dgvHistorial.AllowUserToAddRows = false;
+            this.dgvHistorial.AllowUserToDeleteRows = false;
+            this.dgvHistorial.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistorial.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colEuro,
+            this.colDolar,
+            this.colPeso,
+            this.colFecha});
+            this.dgvHistorial.Location = new System.Drawing.Point(40, 200);
+            this.dgvHistorial.Name = "dgvHistorial";
+            this.dgvHistorial.ReadOnly = true;
+            this.dgvHistorial.Size = new System.Drawing.Size(540, 180);
+            this.dgvHistorial.TabIndex = 9;
+            // 
+            // colEuro
+            // 
+            this.colEuro.HeaderText = "Euro";
+            this.colEuro.Name = "colEuro";
+            this.colEuro.ReadOnly = true;
+            // 
+            // colDolar
+            // 
+            this.colDolar.HeaderText = "Dólar";
+            this.colDolar.Name = "colDolar";
+            this.colDolar.ReadOnly = true;
+            // 
+            // colPeso
+            // 
+            this.colPeso.HeaderText = "Peso";
+            this.colPeso.Name = "colPeso";
+            this.colPeso.ReadOnly = true;
+            // 
+            // colFecha
+            // 
+            this.colFecha.HeaderText = "Fecha y Hora";
+            this.colFecha.Name = "colFecha";
+            this.colFecha.ReadOnly = true;
+            this.colFecha.Width = 150;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.Location = new System.Drawing.Point(160, 20);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(251, 21);
+            this.lblTitulo.TabIndex = 10;
+            this.lblTitulo.Text = "Conversor de Monedas (Euro, $)";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(640, 420);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.dgvHistorial);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtDolar);
+            this.Controls.Add(this.txtEuro);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblDolar);
+            this.Controls.Add(this.lblEuro);
+            this.Name = "Form1";
+            this.Text = "Conversor de Monedas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistorial)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEuro;
+        private System.Windows.Forms.Label lblDolar;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.TextBox txtEuro;
+        private System.Windows.Forms.TextBox txtDolar;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dgvHistorial;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEuro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDolar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPeso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFecha;
+    }
+}
