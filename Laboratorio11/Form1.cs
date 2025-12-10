a31a4214bee6a73089ad2987f08737453c65e0f1
ivan777cordoba <ivan7cordoba@gmail.com>
2025-12-09 21:40:14 -0500

    cambios

---
+++ Laboratorio11/Form1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows.Forms;
+
+namespace Laboratorio11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickThis_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World!";
+        }
+    }
+}   
